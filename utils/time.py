db6448c9e6e8202ecd4a048d71a9efeadf9e9ad5
Jaden Sloan <jaden@Jadens-MacBook-Pro.attlocal.net>
2025-09-07 17:43:00 -0400

    Compare listing age

--- utils/time.py
+++ utils/time.py
@@ -1,1 +1,22 @@
-""
+from datetime import datetime, timezone
+
+
+def listing_age_time(posted_time):
+    posted = datetime.fromisoformat(posted_time.replace("Z", "+00:00"))
+    today = datetime.now(timezone.utc) 
+    days_old = (today - posted).days
+
+    return days_old < 180 
+
+
+def create_time_delta(posted_time, bumped_time):
+    posted_dt = datetime.fromisoformat(posted_time.replace("Z", "+00:00"))
+    bumped_dt = datetime.fromisoformat(bumped_time.replace("Z", "+00:00")) 
+                                    
+
+
+posted_time="2025-08-29T18:26:51.005Z"
+posted = datetime.fromisoformat(posted_time.replace("Z", "+00:00"))
+
+
+print(listing_age_time(posted_time))
